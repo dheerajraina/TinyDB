a438184863b891e9d859268b4a619003a6b23a4c
Dheeraj Raina <dheerajraina@Siva.local>
2026-02-25 21:23:06 +0530

    preliminary database manager added

--- main.cpp
+++ main.cpp
@@ -9,49 +9,82 @@ using namespace std;
 
 string trim(const std::string &s)
 {
-   auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c){return std::isspace(c);});
-   auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c){return std::isspace(c);}).base();
-   return (wsback<=wsfront ? std::string() : std::string(wsfront, wsback));
+    auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c)
+                                    { return std::isspace(c); });
+    auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c)
+                                   { return std::isspace(c); })
+                      .base();
+    return (wsback <= wsfront ? std::string() : std::string(wsfront, wsback));
 }
 
-string to_upper(string s) {
+string to_upper(string s)
+{
     transform(s.begin(), s.end(), s.begin(), ::toupper);
     return s;
 }
 
-struct Column{
+struct Column
+{
     string name;
     string type;
 };
 
-struct Table{
+struct Table
+{
     string name;
-    vector <Column> columns;
+    vector<Column> columns;
 
-    Table(const string& name, const vector<Column>& cols)
-    : name(name),columns(cols){}
+    Table(const string &name, const vector<Column> &cols)
+        : name(name), columns(cols) {}
 };
 
-
-struct Database{
+struct Database
+{
     string name;
-    unordered_map<string,Table>tables;
+    unordered_map<string, Table> tables;
 
-    Database(const string& name):name(name){}
-    
+    Database(const string &name) : name(name) {}
 
-    void create_table(const string& table_name,const vector<Column>& cols){
-        if (tables.count(table_name)){
-            cout<<"Error: Table already exists\n";
+    void create_table(const string &table_name, const vector<Column> &cols)
+    {
+        if (tables.count(table_name))
+        {
+            cout << "Error: Table already exists\n";
             return;
         }
 
-        tables.emplace(table_name,Table(table_name,cols));
-        cout<<"Table '"<<table_name<<"' created\n";
+        tables.emplace(table_name, Table(table_name, cols));
+        cout << "Table '" << table_name << "' created\n";
     }
 };
 
+class DatabaseManager
+{
+private:
+    unordered_map<string, Database> databases;
+    Database *current_db = nullptr;
 
+public:
+    void create_database(const string &name)
+    {
+        if (databases.count(name))
+        {
+            cout << "Error: Database already exists\n";
+            return;
+        }
+        databases.emplace(name, Database(name));
+        cout << "Database '" << name << "' created\n";
+    }
+
+    void use_database(const string& name){
+         if (databases.count(name)) {
+            cout << "Error: Database already exists\n";
+            return;
+        }
+        databases.emplace(name, Database(name));
+        cout << "Database '" << name << "' created\n";
+    }
+};
 
 int main()
 {
@@ -65,15 +98,13 @@ int main()
         cout << ">>";
         getline(cin, line);
         input += line;
-        cout<<"line -> "<<trim(input);
+        cout << "line -> " << trim(input);
 
         if (input.find(';') == string::npos)
             continue;
-        
 
-        if (to_upper(trim(input))=="EXIT;")
-        break;
-
+        if (to_upper(trim(input)) == "EXIT;")
+            break;
 
         input.clear();
     }
