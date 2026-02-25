f178672465338cd2744372c8320147fee48a0618
Dheeraj Raina <dheerajraina@Siva.local>
2026-02-25 19:48:15 +0530

    simple command input

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string trim(const std::string &s)
+{
+   auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c){return std::isspace(c);});
+   auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c){return std::isspace(c);}).base();
+   return (wsback<=wsfront ? std::string() : std::string(wsfront, wsback));
+}
+
+string to_upper(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::toupper);
+    return s;
+}
+
+int main()
+{
+
+    string input, line;
+    cout << "Welcome to TinyDB (type 'exit;')\n";
+
+    while (true)
+    {
+
+        cout << ">>";
+        getline(cin, line);
+        input += line;
+        cout<<"line -> "<<trim(input);
+
+        if (input.find(';') == string::npos)
+            continue;
+        
+
+        if (to_upper(trim(input))=="EXIT;")
+        break;
+
+
+        input.clear();
+    }
+
+    return 0;
+}
